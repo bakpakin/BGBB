6222ca450059dc6a97e6867d98ebf7ce40d0a687
bakpakin <calsrose@gmail.com>
2016-10-06 21:43:37 -0400

    Initial Commit

---
+++ BGBB/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BGBB
+//
+//  Created by Calvin Rose on 10/6/16.
+//  Copyright © 2016 Calvin Rose. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
