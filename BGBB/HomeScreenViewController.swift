27dd2f374c2c66f3fc9390a7b7981f14e85d428b
Nathan Canterbury <nathancanterbury217@gmail.com>
2017-03-25 20:00:08 -0400

    done login screen, started home

---
+++ BGBB/HomeScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeScreenViewController.swift
+//  BGBB
+//
+//  Created by Nathan Canterbury on 3/25/17.
+//  Copyright © 2017 Calvin Rose. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController : UIViewController {
+    
+}
--- BGBB/LoginViewController.swift
+++ BGBB/LoginViewController.swift
@@ -8,6 +8,34 @@
 
 import UIKit
 
-class LoginViewController : UIViewController {
+class LoginViewController : UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var myPicker: UIPickerView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var browseButton: UIButton!
+    
+    let pickerData = ["Harp and Bard", "B&G Drillers", "Fenway Franks", "Cool", "Wow"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myPicker.dataSource = self
+        myPicker.delegate = self
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        myLabel.text = pickerData[row]
+    }
+
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
     
 }
